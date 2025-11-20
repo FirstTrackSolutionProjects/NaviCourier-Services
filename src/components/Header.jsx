3f6dcf3ecdf441300d563be329d634c57ba4eb84
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-11-20 17:22:58 +0530

    chore: landing page UI fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,13 @@
-import NavItem from "./NavItem";
-import { navItems } from "../Constants";
-import {  useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import WalletRechargeModal from "./WalletRechargeModal";
 import { useNavigate, Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useWallet } from "../context/WalletContext";
 
 export default function Header() {
   const navigate = useNavigate();
-  const [showRecharge, setShowRecharge] = useState(false)
   const {verified, isAuthenticated, logout, business_name} = useAuth()
-  const { balance, refreshBalance } = useWallet();
-  const [isMenu,setIsMenu] = useState(false)
-  const closeRechargeModal = () => {
-    setShowRecharge(false);
-  }
-  const toggleMenu = () => {
-    setIsMenu(!isMenu);
-    }
-  useEffect(()=>{
-    if (!verified) return;
-    refreshBalance();
-  },[isAuthenticated])
-
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  };
 
   return (
     <>
-    {showRecharge && <WalletRechargeModal onClose={closeRechargeModal}/>}
-    <section className="relative bg-gray-100 mt-5">
+    <section className="relative bg-gray-100">
       {/* Background Image */}
       <div className="relative h-[500px] w-full">
         <img
